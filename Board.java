8f141c1f75b81f9a7fbf970045be920c24c10255
lexpapaioannou <papaioannou.alexander1@gmail.com>
2016-12-02 02:04:28 -0600

    Add files via upload

--- Board.java
+++ Board.java
@@ -29,7 +29,7 @@ public class Board {
     }
 */
     public String toString() {
-        return board[0] + "  | " + board[1] + " | " + board[2] + " \n-----------\n " + board[3] + " | " + board[4]+" | "+board[5]+" \n-----------\n "+board[6]+" | "+board[7]+" | "+board[8];
+        return " " + board[0] + "  | " + board[1] + " | " + board[2] + " \n-----------\n " + board[3] + " | " + board[4]+" | "+board[5]+" \n-----------\n "+board[6]+" | "+board[7]+" | "+board[8];
     }
 
 }
